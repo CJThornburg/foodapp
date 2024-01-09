4e2449f8fa922c7b544544b7c4afb4a181b2ae50
Cjthornburg <Cjthornburg@thornburgsfamily.com>
2024-01-09 17:27:53 -0500

    first commit

---
+++ app/meals/page.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+export default function Meals() {
+    return (
+      <main>
+        <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Meals
+        </h1>
+        <ul>
+    <li>
+
+    <Link href="/" style={{ color: 'white', textAlign: 'center' }}>Home</Link>
+
+    </li>
+    <li>
+       <Link href="/meals" style={{ color: 'white', textAlign: 'center' }}>Meals</Link>
+    </li>
+    <li>
+    <Link href="/meals/share" style={{ color: 'white', textAlign: 'center' }}>Share</Link>
+    </li>
+    <li>
+    <Link href="/community"style={{ color: 'white', textAlign: 'center' }}> Community</Link>
+    </li>
+
+
+    </ul>
+      </main>
+    );
+  }
