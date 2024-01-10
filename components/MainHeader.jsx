795428c15024413f716fe647c9056b3d0a87d54e
Chris <cjthornburg@thornburgsfamily.com>
2024-01-10 11:32:23 -0500

    create MainHeaderBackgroun component and corresponding css module, ChriT, 1-10-24

--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -2,11 +2,12 @@ import Link from 'next/link'
 import Image from 'next/image'
 import logoImg from '@/assets/logo.png'
 import classes from './mainHeader.module.css'
+import MainHeaderBackground from './MainHeaderBackground'
 
 export default function MainHeader() {
     return (
         <>
-            <header className={classes.header}>
+        <MainHeaderBackground />           <header className={classes.header}>
                 <Link className={classes.logo} href='/'>
                     <Image priority src={logoImg} alt='A plate with good looking food on it' />
                     NextLevel Food
